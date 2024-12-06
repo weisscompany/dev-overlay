f34f99007049ff55b22ded1f366494b7b55ca3c5
weisscompany <165127434+weisscompany@users.noreply.github.com>
2024-12-06 19:03:41 +0100

    Update full-overlay.js

--- full-overlay.js
+++ full-overlay.js
@@ -35,15 +35,12 @@ var overlayHTML = `
 </div>`;
 document.body.insertAdjacentHTML("beforeend", overlayHTML);
 
-function updateOverlayValues() {
-  // Calculate load time
+window.onload = function () {
   var loadTime = (window.performance.timing.domContentLoadedEventEnd - window.performance.timing.navigationStart) / 1000;
   document.getElementById("loading-time").innerText = loadTime.toFixed(3);
+};
 
-  // Set user agent
-  document.getElementById("user-agent").innerText = navigator.userAgent;
-
-  // Set current time
+document.addEventListener("DOMContentLoaded", function () {
   var n = new Date(),
     y = n.getFullYear(),
     m = String(n.getMonth() + 1).padStart(2, "0"),
@@ -51,10 +48,5 @@ function updateOverlayValues() {
     h = String(n.getHours()).padStart(2, "0"),
     min = String(n.getMinutes()).padStart(2, "0");
   document.getElementById("version-time").innerText = `${d}.${m}.${y} ${h}:${min} Uhr`;
-}
-
-// Run updates after ensuring DOM is ready
-document.addEventListener("DOMContentLoaded", function () {
-  // Ensure the overlay has been injected before updating values
-  setTimeout(updateOverlayValues, 0);
+  document.getElementById("user-agent").innerText = navigator.userAgent;
 });
